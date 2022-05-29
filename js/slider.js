def20230c3f1f454a336a4911a51e93dc30bb317
erayde <nosenose1434@gmail.com>
2022-05-28 17:47:49 -0700

    https://www.facebook.com/mijavascript

--- js/slider.js
+++ js/slider.js
@@ -2,13 +2,13 @@ const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section");
 let sliderSectionLast = sliderSection[sliderSection.length -1];
 
-const btnLeft = document.querySelector["#btn-left"];
-const btnRight = document.querySelector["#btn-right"];
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
 
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function Next() {
-    let sliderSectionFirst = document.querySelectorAll(".slider__section"); [0];
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 0.5s";
     setTimeout(function(){
@@ -19,13 +19,13 @@ function Next() {
 }
 
 function Prev() {
-    let sliderSectionFirst = document.querySelectorAll(".slider__section"); [0];
-    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[sliderSection.length-1];
+    //let sliderSectionLast = sliderSection[sliderSection.length -1];
     slider.style.marginLeft = "0";
     slider.style.transition = "all 0.5s";
     setTimeout(function(){
         slider.style.transition = "none";
-        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.insertAdjacentElement('afterbegin', sliderSectionFirst);
         slider.style.marginLeft = "-100%";
     }, 500);
 }
